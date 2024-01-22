6058f064daae7de464bceb0c7b3847fc34ad3139
yoboyghxst <143840193+yoboyghxst@users.noreply.github.com>
2024-01-22 06:59:42 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -14,6 +14,7 @@ local _1 = Instance.new("TextButton")
 local UIStroke_6 = Instance.new("UIStroke")
 local UIStroke_7 = Instance.new("UIStroke")
 local UICorner_3 = Instance.new("UICorner")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 local _2 = Instance.new("TextButton")
 local UIStroke_8 = Instance.new("UIStroke")
 local UIStroke_9 = Instance.new("UIStroke")
@@ -38,9 +39,11 @@ local _6 = Instance.new("TextButton")
 local UIStroke_18 = Instance.new("UIStroke")
 local UIStroke_19 = Instance.new("UIStroke")
 local UICorner_9 = Instance.new("UICorner")
-local Open = Instance.new("TextButton")
+local Subtitle_2 = Instance.new("TextLabel")
 local UIStroke_20 = Instance.new("UIStroke")
+local Open = Instance.new("TextButton")
 local UIStroke_21 = Instance.new("UIStroke")
+local UIStroke_22 = Instance.new("UIStroke")
 
 GuiTools.Name = "GuiTools"
 GuiTools.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -49,7 +52,7 @@ Frame.Parent = GuiTools
 Frame.BackgroundColor3 = Color3.fromRGB(184, 184, 184)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.335987419, 0, 0.0740418956, 0)
+Frame.Position = UDim2.new(0.327584088, 0, 0.0740419179, 0)
 Frame.Size = UDim2.new(0.344249189, 0, 0.791738391, 0)
 
 UIStroke.Thickness = 3
@@ -63,8 +66,8 @@ Minimize.Parent = Frame
 Minimize.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 Minimize.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Minimize.BorderSizePixel = 0
-Minimize.Position = UDim2.new(0.71707809, 0, 0.0143202078, 0)
-Minimize.Size = UDim2.new(0, 52, 0, 41)
+Minimize.Position = UDim2.new(0.717077971, 0, 0.0143201938, 0)
+Minimize.Size = UDim2.new(0, 29, 0, 30)
 Minimize.Font = Enum.Font.Unknown
 Minimize.Text = "-"
 Minimize.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -88,8 +91,8 @@ Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Title.BackgroundTransparency = 1.000
 Title.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Title.BorderSizePixel = 0
-Title.Position = UDim2.new(-0.00152743666, 0, -0.00124887796, 0)
-Title.Size = UDim2.new(0, 318, 0, 53)
+Title.Position = UDim2.new(-0.00152746867, 0, -0.00124888564, 0)
+Title.Size = UDim2.new(0, 169, 0, 41)
 Title.Font = Enum.Font.FredokaOne
 Title.Text = "Blader Ballz"
 Title.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -106,8 +109,8 @@ Subtitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Subtitle.BackgroundTransparency = 1.000
 Subtitle.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Subtitle.BorderSizePixel = 0
-Subtitle.Position = UDim2.new(0.0152417952, 0, 0.144413322, 0)
-Subtitle.Size = UDim2.new(0, 303, 0, 28)
+Subtitle.Position = UDim2.new(0.015241785, 0, 0.144413337, 0)
+Subtitle.Size = UDim2.new(0, 242, 0, 26)
 Subtitle.Font = Enum.Font.FredokaOne
 Subtitle.Text = "Different scripts for Blade Ball!"
 Subtitle.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -123,8 +126,8 @@ _1.Parent = Frame
 _1.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 _1.BorderColor3 = Color3.fromRGB(0, 0, 0)
 _1.BorderSizePixel = 0
-_1.Position = UDim2.new(0.0558252186, 0, 0.261602819, 0)
-_1.Size = UDim2.new(0, 178, 0, 37)
+_1.Position = UDim2.new(0, 13, 0, 74)
+_1.Size = UDim2.new(0, 114, 0, 26)
 _1.Font = Enum.Font.Unknown
 _1.Text = "REDz Hub"
 _1.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -142,13 +145,16 @@ UIStroke_7.Parent = _1
 UICorner_3.CornerRadius = UDim.new(0, 5)
 UICorner_3.Parent = _1
 
+UIAspectRatioConstraint.Parent = _1
+UIAspectRatioConstraint.AspectRatio = 4.811
+
 _2.Name = "2"
 _2.Parent = Frame
 _2.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 _2.BorderColor3 = Color3.fromRGB(0, 0, 0)
 _2.BorderSizePixel = 0
-_2.Position = UDim2.new(0.515221953, 0, 0.261602819, 0)
-_2.Size = UDim2.new(0, 178, 0, 37)
+_2.Position = UDim2.new(0.559974611, 0, 0.259625614, 0)
+_2.Size = UDim2.new(0, 99, 0, 23)
 _2.Font = Enum.Font.Unknown
 _2.Text = "NO GUI AutoParry"
 _2.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -172,7 +178,7 @@ _3.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 _3.BorderColor3 = Color3.fromRGB(0, 0, 0)
 _3.BorderSizePixel = 0
 _3.Position = UDim2.new(0.0558252186, 0, 0.397472382, 0)
-_3.Size = UDim2.new(0, 178, 0, 37)
+_3.Size = UDim2.new(0, 114, 0, 26)
 _3.Font = Enum.Font.Unknown
 _3.Text = "BEDOL Hub (RECCOMENDED)"
 _3.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -195,8 +201,8 @@ Close.Parent = Frame
 Close.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 Close.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Close.BorderSizePixel = 0
-Close.Position = UDim2.new(0.858609438, 0, 0.0143202078, 0)
-Close.Size = UDim2.new(0, 52, 0, 41)
+Close.Position = UDim2.new(0.874883115, 0, 0.0108117461, 0)
+Close.Size = UDim2.new(0, 30, 0, 32)
 Close.Font = Enum.Font.Unknown
 Close.Text = "X"
 Close.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -219,8 +225,8 @@ _4.Parent = Frame
 _4.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 _4.BorderColor3 = Color3.fromRGB(0, 0, 0)
 _4.BorderSizePixel = 0
-_4.Position = UDim2.new(0.515221953, 0, 0.397472382, 0)
-_4.Size = UDim2.new(0, 178, 0, 37)
+_4.Position = UDim2.new(0.555906415, 0, 0.397472382, 0)
+_4.Size = UDim2.new(0, 99, 0, 26)
 _4.Font = Enum.Font.Unknown
 _4.Text = "X7 Parry"
 _4.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -243,8 +249,8 @@ _5.Parent = Frame
 _5.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 _5.BorderColor3 = Color3.fromRGB(0, 0, 0)
 _5.BorderSizePixel = 0
-_5.Position = UDim2.new(0.0558252186, 0, 0.525189757, 0)
-_5.Size = UDim2.new(0, 178, 0, 37)
+_5.Position = UDim2.new(0.0517566986, 0, 0.546240509, 0)
+_5.Size = UDim2.new(0, 114, 0, 26)
 _5.Font = Enum.Font.Unknown
 _5.Text = "ZapHub (KEY)"
 _5.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -267,8 +273,8 @@ _6.Parent = Frame
 _6.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
 _6.BorderColor3 = Color3.fromRGB(0, 0, 0)
 _6.BorderSizePixel = 0
-_6.Position = UDim2.new(0.515221953, 0, 0.525189757, 0)
-_6.Size = UDim2.new(0, 178, 0, 37)
+_6.Position = UDim2.new(0.555906177, 0, 0.546240509, 0)
+_6.Size = UDim2.new(0, 99, 0, 26)
 _6.Font = Enum.Font.Unknown
 _6.Text = "Peris Hub"
 _6.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -286,6 +292,24 @@ UIStroke_19.Parent = _6
 UICorner_9.CornerRadius = UDim.new(0, 5)
 UICorner_9.Parent = _6
 
+Subtitle_2.Name = "Subtitle"
+Subtitle_2.Parent = Frame
+Subtitle_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Subtitle_2.BackgroundTransparency = 1.000
+Subtitle_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Subtitle_2.BorderSizePixel = 0
+Subtitle_2.Position = UDim2.new(0.0071048243, 0, 0.877680242, 0)
+Subtitle_2.Size = UDim2.new(0, 242, 0, 26)
+Subtitle_2.Font = Enum.Font.FredokaOne
+Subtitle_2.Text = "MOBILE VERSION"
+Subtitle_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Subtitle_2.TextScaled = true
+Subtitle_2.TextSize = 14.000
+Subtitle_2.TextWrapped = true
+
+UIStroke_20.Thickness = 3
+UIStroke_20.Parent = Subtitle_2
+
 Open.Name = "Open"
 Open.Parent = GuiTools
 Open.BackgroundColor3 = Color3.fromRGB(182, 182, 182)
@@ -301,14 +325,14 @@ Open.TextScaled = true
 Open.TextSize = 14.000
 Open.TextWrapped = true
 
-UIStroke_20.Thickness = 4
-UIStroke_20.Parent = Open
-
-UIStroke_21.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke_21.Thickness = 4
 UIStroke_21.Parent = Open
 
-local function GWNXZ_fake_script() 
+UIStroke_22.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+UIStroke_22.Thickness = 4
+UIStroke_22.Parent = Open
+
+local function YYPQGS_fake_script() -- Minimize.LocalScript 
 	local script = Instance.new('LocalScript', Minimize)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -316,8 +340,8 @@ local function GWNXZ_fake_script()
 		script.Parent.Parent.Parent.Open.Visible = true
 	end)
 end
-coroutine.wrap(GWNXZ_fake_script)()
-local function OEZP_fake_script() -- _1.LocalScript 
+coroutine.wrap(YYPQGS_fake_script)()
+local function JXUZYQ_fake_script() -- _1.LocalScript 
 	local script = Instance.new('LocalScript', _1)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -325,8 +349,8 @@ local function OEZP_fake_script() -- _1.LocalScript
 		print("Executed Script!")
 	end)
 end
-coroutine.wrap(OEZP_fake_script)()
-local function FRMQI_fake_script() -- _2.LocalScript 
+coroutine.wrap(JXUZYQ_fake_script)()
+local function VHJRWC_fake_script() -- _2.LocalScript 
 	local script = Instance.new('LocalScript', _2)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -334,31 +358,25 @@ local function FRMQI_fake_script() -- _2.LocalScript
 		print("Executed Script!")
 	end)
 end
-coroutine.wrap(FRMQI_fake_script)()
-local function URRUH_fake_script() -- _3.LocalScript 
+coroutine.wrap(VHJRWC_fake_script)()
+local function HTLET_fake_script() -- _3.LocalScript 
 	local script = Instance.new('LocalScript', _3)
 
 	script.Parent.MouseButton1Click:Connect(function()
-		game:GetService("ReplicatedStorage").Security.RemoteEvent:Destroy()
-		game:GetService("ReplicatedStorage").Security[""]:Destroy()
-		game:GetService("ReplicatedStorage").Security:Destroy()
-		game:GetService("Players").LocalPlayer.PlayerScripts.Client.DeviceChecker:Destroy()
-		wait(1)
-		loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Bedol-hub-next-generation-8628"))()
-		
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/-beta-/main/AutoParry.lua"))()
 		print("Executed Script!")
 	end)
 end
-coroutine.wrap(URRUH_fake_script)()
-local function CTETM_fake_script() -- Close.LocalScript 
+coroutine.wrap(HTLET_fake_script)()
+local function AQUIS_fake_script() -- Close.LocalScript 
 	local script = Instance.new('LocalScript', Close)
 
 	script.Parent.MouseButton1Click:Connect(function()
 		script.Parent.Parent.Parent:Destroy()
 	end)
 end
-coroutine.wrap(CTETM_fake_script)()
-local function BINS_fake_script() -- Frame.DragScript 
+coroutine.wrap(AQUIS_fake_script)()
+local function PYPTRNP_fake_script() -- Frame.DragScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UIS = game:GetService('UserInputService')
@@ -397,8 +415,8 @@ local function BINS_fake_script() -- Frame.DragScript
 	end)
 	
 end
-coroutine.wrap(BINS_fake_script)()
-local function PBRSZ_fake_script() -- _4.LocalScript 
+coroutine.wrap(PYPTRNP_fake_script)()
+local function WWLSG_fake_script() -- _4.LocalScript 
 	local script = Instance.new('LocalScript', _4)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -407,8 +425,8 @@ local function PBRSZ_fake_script() -- _4.LocalScript
 		print("Executed Script!")
 	end)
 end
-coroutine.wrap(PBRSZ_fake_script)()
-local function LQETNP_fake_script() -- _5.LocalScript 
+coroutine.wrap(WWLSG_fake_script)()
+local function SRXN_fake_script() -- _5.LocalScript 
 	local script = Instance.new('LocalScript', _5)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -417,8 +435,8 @@ local function LQETNP_fake_script() -- _5.LocalScript
 		print("Executed Script!")
 	end)
 end
-coroutine.wrap(LQETNP_fake_script)()
-local function JJSQ_fake_script() -- _6.LocalScript 
+coroutine.wrap(SRXN_fake_script)()
+local function JVMH_fake_script() -- _6.LocalScript 
 	local script = Instance.new('LocalScript', _6)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -427,8 +445,8 @@ local function JJSQ_fake_script() -- _6.LocalScript
 		print("Executed Script!")
 	end)
 end
-coroutine.wrap(JJSQ_fake_script)()
-local function BKQZUHI_fake_script() -- Open.DragScript 
+coroutine.wrap(JVMH_fake_script)()
+local function CMCLP_fake_script() -- Open.DragScript 
 	local script = Instance.new('LocalScript', Open)
 
 	local UIS = game:GetService('UserInputService')
@@ -467,8 +485,8 @@ local function BKQZUHI_fake_script() -- Open.DragScript
 	end)
 	
 end
-coroutine.wrap(BKQZUHI_fake_script)()
-local function OWYIGU_fake_script() -- Open.LocalScript 
+coroutine.wrap(CMCLP_fake_script)()
+local function YODNI_fake_script() -- Open.LocalScript 
 	local script = Instance.new('LocalScript', Open)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -476,4 +494,4 @@ local function OWYIGU_fake_script() -- Open.LocalScript
 		script.Parent.Visible = false
 	end)
 end
-coroutine.wrap(OWYIGU_fake_script)()
+coroutine.wrap(YODNI_fake_script)()
